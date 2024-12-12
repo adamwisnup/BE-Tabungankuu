79e824deed72051dcae72bb1e7e64d092b6c87e4
adamwisnup <wisnuadam21@gmail.com>
2024-12-12 16:43:01 +0700

    refactor: recreate 'tabungan' table with 'type' and 'saldo' columns

--- prisma/migrations/20241130014343_add_saldo_and_type/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `type` to the `tabungan` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "tabungan" ADD COLUMN     "saldo" INTEGER,
-ADD COLUMN     "type" TEXT NOT NULL;
--- prisma/migrations/20241212094215_init/migration.sql
+++ prisma/migrations/20241212094215_init/migration.sql
@@ -2,6 +2,8 @@
 CREATE TABLE "tabungan" (
     "id" TEXT NOT NULL,
     "uang" INTEGER NOT NULL,
+    "type" TEXT NOT NULL,
+    "saldo" INTEGER,
     "waktu" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "tabungan_pkey" PRIMARY KEY ("id")
